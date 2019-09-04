29d4a57f937e3c6110374eaa4d194d6496bdc712
Luis Cabrera Benito <parzibyte@gmail.com>
2019-09-04 11:17:33 -0500

    El total no se mostraba cuando la cantidad era 1
    Así que en lugar de estar invocando a refrescarTotal a cada momento, getTotal hace la operación

--- src/main/java/me/parzibyte/sistemaventasspringboot/ProductoParaVender.java
+++ src/main/java/me/parzibyte/sistemaventasspringboot/ProductoParaVender.java
@@ -1,7 +1,7 @@
 package me.parzibyte.sistemaventasspringboot;
 
 public class ProductoParaVender extends Producto {
-    private Float cantidad, total;
+    private Float cantidad;
 
     public ProductoParaVender(String nombre, String codigo, Float precio, Float existencia, Integer id, Float cantidad) {
         super(nombre, codigo, precio, existencia, id);
@@ -11,16 +11,10 @@ public class ProductoParaVender extends Producto {
     public ProductoParaVender(String nombre, String codigo, Float precio, Float existencia, Float cantidad) {
         super(nombre, codigo, precio, existencia);
         this.cantidad = cantidad;
-        this.refrescarTotal();
-    }
-
-    public void refrescarTotal() {
-        this.total = this.cantidad * this.getPrecio();
     }
 
     public void aumentarCantidad() {
         this.cantidad++;
-        this.refrescarTotal();
     }
 
     public Float getCantidad() {
@@ -28,6 +22,6 @@ public class ProductoParaVender extends Producto {
     }
 
     public Float getTotal() {
-        return total;
+        return this.getPrecio() * this.cantidad;
     }
 }
